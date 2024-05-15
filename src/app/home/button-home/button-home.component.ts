d791617a590d8bc8f04ad9db53eb8e7e10b51fd5
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-05-15 15:59:30 -0300

    component button-home created empty

---
+++ src/app/home/button-home/button-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-home',
+  templateUrl: './button-home.component.html',
+  styleUrls: ['./button-home.component.css']
+})
+export class ButtonHomeComponent {
+
+}
